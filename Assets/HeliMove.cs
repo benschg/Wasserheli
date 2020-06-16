8ba260416970b48081d949d25226306222a05941
Benjamin Grauer <grauerb@gmail.com>
2020-06-16 21:19:14 +0200

    Add initial Wasserheli

---
+++ Assets/HeliMove.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeliMove : MonoBehaviour
+{
+    public GameObject playerModel;
+
+    int straveOn = 0;
+    float straveSpeed = 0;
+    float straveSpeedMax = 30.0f;
+
+    int moveForwardOn = 0;
+    float forwardSpeed = 0;
+    float forwardSpeedMax = 30.0f;
+
+    int updownOn = 0;
+    float updownSpeed = 0;
+    float updownSpeedMax = 10.0f;
+
+    float mouseMovement = 0;
+    float travelDirection = 0.0f;
+
+    public Joystick movementJoystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.CheckKeys();
+        this.GetMouseMovements();
+
+        straveSpeed = Mathf.Lerp(straveSpeed, straveSpeedMax * straveOn, 5.0f * Time.deltaTime);
+        forwardSpeed = Mathf.Lerp(forwardSpeed, forwardSpeedMax * moveForwardOn, 5.0f * Time.deltaTime);
+        updownSpeed = Mathf.Lerp(updownSpeed, updownSpeedMax * updownOn, 5.0f * Time.deltaTime);
+        travelDirection += mouseMovement;
+
+    
+    
+        Vector3 movement = new Vector3(forwardSpeed * Time.deltaTime,
+                                        updownSpeed * Time.deltaTime,
+                                        straveSpeed * Time.deltaTime);
+        Quaternion travelDirectionQ = Quaternion.Euler(new Vector3(0, travelDirection, 0));
+
+        this.transform.position += travelDirectionQ * movement;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0, travelDirection, 0));
+
+        playerModel.transform.localRotation = Quaternion.Euler(new Vector3(straveSpeed, 0, 0)) 
+                                         * Quaternion.Euler(new Vector3(0,0,-forwardSpeed));
+                                    
+        
+        //Debug.Log($" ${Time.deltaTime} - ${straveSpeed} - ${position.z}");
+    }
+
+    void CheckKeys()
+    {
+        DirectionalKeys(KeyCode.A, KeyCode.D, ref this.straveOn);
+        DirectionalKeys(KeyCode.W, KeyCode.S, ref this.moveForwardOn);
+        DirectionalKeys(KeyCode.F, KeyCode.R, ref this.updownOn);
+    }
+
+    void GetMouseMovements()
+    {
+        this.mouseMovement = Input.GetAxis("Mouse X");
+
+    }
+
+
+    bool DirectionalKeys(KeyCode minus, KeyCode plus, ref int onOff)
+    {
+        if (Input.GetKeyDown(minus))
+        {
+            onOff = -1;
+            return true;
+        }
+        else if (Input.GetKeyDown(plus))
+        {
+            onOff = 1;
+            return true;
+        }
+        else if ((Input.GetKeyUp(minus) && onOff == -1) ||
+            (Input.GetKeyUp(plus) && onOff == 1))
+        {   
+            onOff = 0;
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ Assets/TrackingCamera.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackingCamera : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    float distance;
+
+    Vector3 playerPrevPos, playerMoveDir;
+
+    Vector3 camOffset;
+
+    float mouseVerticalMovement = 0.0f;
+    float verticalOffset = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+        camOffset = new Vector3(20, 5, 0);
+        offset = transform.position - player.transform.position;
+
+        distance = offset.magnitude;
+        playerPrevPos = player.transform.position;
+    }
+
+    void LateUpdate () {
+        this.GetMouseMovements();
+
+        verticalOffset += mouseVerticalMovement * .8f;
+        if (verticalOffset > 10) { verticalOffset = 10; }
+        else if (verticalOffset < -10) { verticalOffset = -10; }
+
+        camOffset.y = this.verticalOffset;
+
+        Vector3 playerDir = player.transform.forward;
+
+        Vector3 offset = player.transform.rotation * camOffset;
+
+        this.transform.position = Vector3.Lerp(this.transform.position, player.transform.position + offset, 2.0f * Time.deltaTime);
+        // playerMoveDir = player.transform.position - playerPrevPos;
+        // if (playerMoveDir != Vector3.zero)
+        // {
+        //     playerMoveDir = playerMoveDir.normalized;
+        //     transform.position = player.transform.position - playerMoveDir * distance;
+
+        //     transform.position += new Vector3(0, transform.position.y + 5.0f, 0); // required height
+
+             transform.LookAt(player.transform.position);
+        // }
+    }
+
+    void GetMouseMovements()
+    {
+        this.mouseVerticalMovement = Input.GetAxis("Mouse Y");
+
+    }
+}
